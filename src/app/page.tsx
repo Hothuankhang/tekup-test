5c01f9e83e72f31f25ca365a0711001c0001d797
Hothuankhang <khang.ho@madison-technology.com>
2023-09-18 03:45:47 +0700

    feat/setting_skeleton

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { Button } from '@chakra-ui/react'
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <Button color={"red"}>ec</Button>
+  )
+}
